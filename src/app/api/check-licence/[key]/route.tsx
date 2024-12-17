da86f70312767090ce69bbca75957ecfb900ee45
Nils Dohms (ET) <nils.dohms@engelmann.training>
2024-12-17 14:15:18 +0100

    + Licence API

---
+++ src/app/api/check-licence/[key]/route.tsx
@@ -0,0 +1,78 @@
+import {NextResponse} from "next/server";
+import {headers} from "next/headers";
+import moment from "moment";
+
+export async function POST(request: Request, {params}: { params: { key: string } }) {
+    const {key} = await params
+    const header = await headers()
+    let requestBody
+    try {
+        requestBody = await request.json()
+    } catch (error) {
+        return new NextResponse('Your body is malformed!', {status: 500});
+    }
+
+    // Check for key
+    if (!key) {
+        return new NextResponse('A licence key is required!', {status: 400});
+    }
+    // Check if licence Exists
+    const licenceToCheck = licences.find(licence => licence.licence == key)
+    if (!licenceToCheck) {
+        return new NextResponse('Couldn\'t find the provided licence key!', {status: 404});
+    }
+    // Check Version
+    if (!requestBody.version) {
+        return new NextResponse('Your GBU-KMU-Version must be provided!', {status: 400});
+    }
+
+    if (requestBody.version.substring(0, 1) != licenceToCheck.version) {
+        return new NextResponse('Your license-version is not suitable for the version of your GBU-KMU!', {status: 409});
+    }
+    // Check ServerRef
+    const host = header.get('x-forwarded-for')
+    if (!host) {
+        console.log('Licence check failed for host: ' + host);
+        return new NextResponse('Your host couldn\'t be detected!', {status: 404});
+    }
+
+    if (host != licenceToCheck.serverRef) {
+        console.log('Licence check failed for licence [' + licenceToCheck.licence + '] for host: ' + host);
+        return new NextResponse('Your licence dosen\'t match your host!', {status: 403});
+    }
+
+    // Check Company max
+    if (!requestBody.companies) {
+        return new NextResponse('The current number of companies must be provided!', {status: 400});
+    }
+
+    if (requestBody.companies > licenceToCheck.companyLimit) {
+        return new NextResponse('You have reached your company limit!', {status: 418});
+    }
+
+    return NextResponse.json({
+            expires: licenceToCheck.expirationDate,
+            companyLimit: licenceToCheck.companyLimit,
+            version: licenceToCheck.version,
+        },
+        {
+            status: 200,
+            headers: {
+                'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                'Pragma': 'no-cache',
+                'Expires': '0'
+            }
+        });
+}
+
+const licences = [
+    {
+        licence: "9022fcf6-a056-4df4-8284-bac65ef9ad2a",
+        customer: "Maicon GmbH",
+        contact: "Maicon GmbH",
+        expirationDate: moment('2025-12-24'),
+        version: "1",
+        serverRef: "::1",
+        companyLimit: 20
+    },
+]
